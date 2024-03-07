23384bc2ca55c725447e66e0540dad8943c23d9b
Sabhaya Neharika Himmatbhai <sabhayaneharikahimmatbhai@Sabhayas-MacBook-Air.local>
2024-03-07 16:38:13 +0530

    navbar updated

--- src/navbar.js
+++ src/navbar.js
@@ -31,45 +31,78 @@ function Navbar() {
 
   return (
     <>
-      <nav>
-        <ul className="navbar">
+     <nav className="navbar navbar-expand-lg ">
+      <div className="container-fluid">
+        <Link to="/">
           <img src={logo} style={{ height: 100, width: 200 }} alt="logo" />
-          <li className="nav-item">
-            <Link to="/">HOME</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about">ABOUT</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/projects">PROJECTS</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/news">NEWS</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/shop">SHOPS</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact">CONTACT</Link>
-          </li>
-          {user ? (
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <button className="nav-link" onClick={handleLogout}>
-                LOGOUT
-              </button>
+              <Link to="/" className="nav-link">
+                HOME
+              </Link>
             </li>
-          ) : (
-            <>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                ABOUT
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/projects" className="nav-link">
+                PROJECTS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/news" className="nav-link">
+                NEWS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/shop" className="nav-link">
+                SHOPS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">
+                CONTACT
+              </Link>
+            </li>
+            {user ? (
               <li className="nav-item">
-                <Link to="/signup">SIGNUP</Link>
+                <button className="nav-link" onClick={handleLogout}>
+                  LOGOUT
+                </button>
               </li>
-              <li className="nav-item">
-                <Link to="/login">LOGIN</Link>
-              </li>
-            </>
-          )}
-        </ul>
-      </nav>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <Link to="/signup" className="nav-link">
+                    SIGNUP
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/login" className="nav-link">
+                    LOGIN
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
     </>
   );
 }
